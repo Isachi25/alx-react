6b7b80776ad4b147d18cdf9b3637ae17f4d045e9
Achieng Masiga <134079083+Isachi25@users.noreply.github.com>
2024-09-17 00:04:34 +0300

    Update dashboard_main.js

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 import _ from 'lodash';
 
+// Add elements to the body in the required order
 $('body').append("<p>Holberton Dashboard</p>");
 $('body').append("<p>Dashboard data for the students</p>");
 $('body').append("<button>Click here to get started</button>");
@@ -8,14 +9,18 @@ $('body').append("<p id='count'></p>");
 $('body').append("<p>Copyright - Holberton School</p>");
 
 let count = 0;
-let infoClick = "";
-let display = document.getElementById('count');
-function updateCounter() {
-    count++;
-    infoClick = `${count} clicks on the button`;
-    display.innerHTML = infoClick;
-};
 
-$('button').on('click', _.debounce(function()  {
-    updateCounter();
+function updateCounter() {
+  count++;
+  $('#count').html(`${count} clicks on the button`);
+  
+  // Special condition for 500 clicks
+  if (count === 500) {
+    alert('You have reached 500 clicks!');
+  }
+}
+
+// Use Lodash's debounce to prevent spamming
+$('button').on('click', _.debounce(function() {
+  updateCounter();
 }, 300));
