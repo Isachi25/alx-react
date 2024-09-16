e893dc345df884f5cae510d25419f9e5c5640921
Isachi25 <sharonachiengmasiga@gmail.com>
2024-09-16 20:39:23 +0000

    Correct updateCounter function

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -3,19 +3,18 @@ import _ from 'lodash';
 
 $('body').append("<p>Holberton Dashboard</p>");
 $('body').append("<p>Dashboard data for the students</p>");
-$('body').append("<button>Click here to get started</button");
+$('body').append("<button>Click here to get started</button>");
 $('body').append("<p id='count'></p>");
 $('body').append("<p>Copyright - Holberton School</p>");
 
 let count = 0;
-let infoClick = "";
-let display = document.getElementById('count');
 function updateCounter() {
     count++;
-    infoClick = `${count} clicks on the button`;
-    display.innerHTML = infoClick;
-};
+    $('#count').html(`${count} clicks on the button`);
+}
 
-$('button').on('click', _.debounce(function()  {
+// Use lodash's debounce to limit the button click function
+$('button').on('click', _.debounce(function() {
     updateCounter();
 }, 300));
+
